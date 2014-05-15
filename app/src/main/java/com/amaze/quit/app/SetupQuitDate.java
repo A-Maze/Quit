81b119edede9b6a7c2ab9d77ba2a75d09cfa35b5
rikvanderwerf <Rikvanderwerf@outlook.com>
2014-05-15 11:45:22 +0200

    reupload van setupquitdate

---
+++ app/src/main/java/com/amaze/quit/app/SetupQuitDate.java
@@ -0,0 +1,44 @@
+package com.amaze.quit.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Date;
+
+/**
+ * Created by Rik on 14-5-2014.
+ */
+public class SetupQuitDate extends Activity{
+
+    /* de next button op het quitdate scherm */
+    private Button nextButton = (Button) findViewById(R.id.bStopDateNext);
+    /* de datepicker op het quitdate scherm */
+    private DatePicker quitDatePicker = (DatePicker) findViewById(R.id.dpStopDatum);
+    // de stopdatum variabelen
+    public int quitDay;
+    public int quitMonth;
+    public int quitYear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_quit_date);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // wat te doen als de Next Button geklikt is.
+                quitDay = quitDatePicker.getDayOfMonth();
+                quitMonth = quitDatePicker.getMonth();
+                quitYear = quitDatePicker.getYear();
+            }
+        });
+
+
+
+
+    }
+}

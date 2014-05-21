a6dbe867acb22ed22eb19936b7951754493ba2f3
Robin Siep <mail@robin-siep.nl>
2014-05-21 16:34:11 +0200

    User Challenge class and a part of the achievement fragment

---
+++ app/src/main/java/com/amaze/quit/app/Achievements.java
@@ -0,0 +1,53 @@
+package com.amaze.quit.app;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class Achievements extends Fragment {
+
+
+    public static final Achievements newInstance() {
+        Achievements f = new Achievements();
+        Bundle bdl = new Bundle(1);
+        f.setArguments(bdl);
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_achievements, container, false);
+        return v;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            //gets the recources
+            Resources res = getResources();
+
+            Activity homeActivity = getActivity();
+
+            //styles the actionbar
+            ActionBar bar = homeActivity.getActionBar();
+            ColorDrawable color = new ColorDrawable(res.getColor(R.color.orange));
+            bar.setBackgroundDrawable(color);
+            //sets the title
+            CharSequence title = res.getString(R.string.title_fragment_achievements);
+            homeActivity.setTitle(title);
+        }
+
+    }
+}

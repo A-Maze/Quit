9f0f4428d896b919e72c3a2b1ba85cb8ec629ed0
Robin Siep <mail@robin-siep.nl>
2014-05-27 21:04:08 +0200

    exception that catches the SQL exception

--- app/src/main/java/com/amaze/quit/app/SplashScreen.java
+++ app/src/main/java/com/amaze/quit/app/SplashScreen.java
@@ -6,6 +6,8 @@ package com.amaze.quit.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 
 
@@ -35,19 +37,39 @@ public class SplashScreen extends Activity {
     }
 
     protected void firstTime(){
+
         // loads the settings
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         Intent startMainActivity;
 
         // if it is the first time opening the app load Setup
-        if(settings.getBoolean("first_time",true)){
+        if(settings.getBoolean("first_time",true) || isDatabaseThere()){
             startMainActivity = new Intent(getApplicationContext(),Setup.class);
             settings.edit().putBoolean("first_time",false).commit();
         }
         else{
+
+
             //else load the main activity
             startMainActivity = new Intent(getApplicationContext(),Home.class);
         }
         startActivity(startMainActivity);
+    }
+
+    //makes sure the user filled in setup
+    private boolean isDatabaseThere() {
+        DatabaseHandler checkDB = null;
+
+        try {
+            checkDB = new DatabaseHandler(this);
+        } catch (SQLiteException e) {
+
+        }
+
+        if (checkDB != null) {
+            checkDB.close();
+        }
+
+        return checkDB != null;
     }
 }
